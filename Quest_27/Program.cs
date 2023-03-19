ad6ba23d38adefe846bb061f7db1e995c9eb7478
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-03-19 22:17:48 +0300

    Решение задачи на нахождения суммы цифр числа

---
+++ Quest_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+// Метод для запроса числа у пользователя
+string number(string text)
+{
+  Console.Write(text);
+  return Console.ReadLine()!;
+}
+// Метод нахождения суммы цифр запрошенного числа
+int Sum(string text)
+{
+  int result = 0;
+  for(int i = 0; i < text.Length; i++)
+  {
+    result = result + (int)Char.GetNumericValue(text[i])!;
+  }
+return result;
+}
+// Исполнительная часть
+string num = number("Введите число ");
+int S = Sum(num);
+Console.Write($"Сумма всех цифр числа равна {S}");
