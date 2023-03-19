5650c5346b6618d5b4bf5aa4189222acd71bfe20
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-03-19 22:43:29 +0300

    Рандомный массив размерностью пользователя

---
+++ Quest_29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// (числа берете любые)
+
+// Запрос размерности массива
+int number(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine()!);
+}
+// Функция массива из рандомных цифр
+int[] Massiv(int size)
+{
+  int[] A = new int[size];
+  for(int i =0; i < size; i++)
+  {
+    A[i] = new Random().Next(0, 10);
+  }
+return A;
+}
+// Исполнительная часть
+int num = number("Введите размерность массива ");
+int[] B = Massiv(num);
+Console.Write($"[{String.Join("; ",B)}]");
+
